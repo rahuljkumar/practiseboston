dc0dd1986330879878b8f1b6f59f7d0053852076
rahuljkumar <132330352+rahuljkumar@users.noreply.github.com>
2024-01-27 18:46:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def predict():
     output=regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="The house price prediction is {}".format(output))
 
+#if __name__=="__main__":
+    #app.run(debug=True)
+
 if __name__=="__main__":
-    app.run(debug=True)
-
-
+    app.run(host='0.0.0.0',port=8080)
